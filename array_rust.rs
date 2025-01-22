736fc11ab8c73fc202d7ac28de85a40a5536029a
ohba <ohba@sendai-nct.ac.jp>
2025-01-22 11:52:11 +0900

    First commit

---
+++ array_rust.rs
@@ -0,0 +1,12 @@
+fn main()
+{
+	let week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+	println!("{:?}",week);
+
+	let array:[f64; 3] = [3.14, 1.6e-19, 6.02e23];
+	println!("0:{}, 1:{}, 2:{}",array[0], array[1], array[2]);
+
+//	println!("3:{}",array[3]);	// これは必ずエラーになる
+
+
+}
