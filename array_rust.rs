59a789cad1355e766fff1292d29cfaa5cb884a5e
ohba <ohba@sendai-nct.ac.jp>
2025-01-28 22:35:16 +0900

    空配列を追加

--- array_rust.rs
+++ array_rust.rs
@@ -6,7 +6,11 @@ fn main()
 	let array:[f64; 3] = [3.14, 1.6e-19, 6.02e23];
 	println!("0:{}, 1:{}, 2:{}",array[0], array[1], array[2]);
 
+
 //	println!("3:{}",array[3]);	// これは必ずエラーになる
 
+	let mut blank:[i32;4]=[0;4];	//[0;4]は0個の4で配列を埋める
+	blank[0] = 11;
+	println!("{:?}",blank);
 
 }
